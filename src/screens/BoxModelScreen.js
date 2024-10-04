04da96b31d9cca46cf3b28f8bf397a97b9c60246
pammu453 <pramodsavant2001@gmail.com>
2024-10-04 20:42:27 +0530

    layout problem solution

--- src/screens/BoxModelScreen.js
+++ src/screens/BoxModelScreen.js
@@ -4,34 +4,35 @@ import { View, Text, StyleSheet } from 'react-native'
 const BoxModelScreen = () => {
     return (
         <View style={styles.parent}>
-            <Text style={styles.child1}>Child #1</Text>
-            <Text style={styles.child2}>Child #2</Text>
-            <Text style={styles.child3}>Child #3</Text>
+            <View style={styles.child1}/>
+            <View style={styles.child2}/>
+            <View style={styles.child3}/>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     parent: {
-        borderWidth: 3,
-        borderColor: "black",
         width: 400,
         height: 500,
+        flexDirection: 'row',
+        justifyContent:"space-between"
     },
     child1: {
         backgroundColor: "orange",
+        width: 100,
+        height: 100
     },
     child2: {
         backgroundColor: "blue",
-        position: "absolute",
-        top: 0,
-        left: 0,
-        bottom: 0,
-        right: 0,
-        // ...StyleSheet.absoluteFillObject  // same as baove 5 property
+        width: 100,
+        height: 100,
+        top:100
     },
     child3: {
         backgroundColor: "green",
+        width: 100,
+        height: 100
     },
 })
 
